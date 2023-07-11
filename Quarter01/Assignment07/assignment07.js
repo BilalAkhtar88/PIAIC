652a2444c0746d69668a44cc4ea2385e0e4cf726
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2023-07-11 22:43:10 +0500

    Assignment 07 Upload

---
+++ Quarter01/Assignment07/assignment07.js
@@ -0,0 +1,8 @@
+console.log("Addition: 4 + 4 = " + (4 + 4));
+console.log("Subtraction: 9 - 1 = ", 9 - 1);
+console.log("Multiplication: 2 * 4 = " + 2 * 4);
+console.log("Division: 16 / 2 = ", 16 / 2);
+//For understanding, try following:
+console.log("Addition: 4 + 4 = " + 4 + 4);
+console.log("Addition: 4 + 4 = ", 4 + 4);
+console.log("Division: 16 / 2 = " + 16 / 2);
