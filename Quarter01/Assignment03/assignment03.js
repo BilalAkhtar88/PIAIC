05563c9c72431114590f057667342c80979fbce6
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2023-07-11 20:59:49 +0500

    Assignment 03 Upload

---
+++ Quarter01/Assignment03/assignment03.js
@@ -0,0 +1,10 @@
+var personName = "MuhammAd bilAl AkHTar";
+console.log("The name in Original text: ", personName);
+//Convert and display person name in lower case
+var nameLowerCase = personName.toLowerCase();
+console.log("The name in Lower case: ", nameLowerCase);
+var nameUpperCase = personName.toUpperCase();
+console.log("The name in Upper case: ", nameUpperCase);
+// to title case
+var nameTitleCase = personName.toLowerCase().split(" ").map(function (l) { return l[0].toUpperCase() + l.substr(1); }).join(" ");
+console.log("The name in Title Case: ", nameTitleCase);
