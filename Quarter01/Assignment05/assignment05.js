69913d6ae6aefcbf319561bbc6263017e4401097
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2023-07-11 22:04:13 +0500

    Assignment 05 Upload

---
+++ Quarter01/Assignment05/assignment05.js
@@ -0,0 +1,13 @@
+var famous_person = "Michele Ruiz";
+//Using either methods below for concatenating strings in Typescript:
+//(Reference https://blog.bitsrc.io/double-quotes-vs-single-quotes-vs-backticks-in-javascript-3cab5aaea55)
+var message = famous_person + " once said, \"If people are doubting how far you can go, go so far that you can’t hear them anymore.\"";
+//let message = famous_person + ' once said, \"If people are doubting how far you can go, go so far that you can’t hear them anymore.\"'; 
+//let message = `${famous_person} once said, "If people are doubting how far you can go, go so far that you can’t hear them anymore."`;
+console.log(message);
+//Ways to concatenate strings in console.log function:
+//console.log(famous_person + " once said, \"If people are doubting how far you can go, go so far that you can’t hear them anymore.\""); 
+//console.log(famous_person , "once said, \"If people are doubting how far you can go, go so far that you can’t hear them anymore.\""); 
+//console.log(`${famous_person} once said, "If people are doubting how far you can go, go so far that you can’t hear them anymore."`); 
+//console.log(`%s once said, "If people are doubting how far you can go, go so far that you can’t hear them anymore."`, famous_person); 
+//console.log('%s once said, "If people are doubting how far you can go, go so far that you can’t hear them anymore."', famous_person); 
